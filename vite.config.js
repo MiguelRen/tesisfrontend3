ce7d8e4f9b1296260ecd39c5b51e5c96698549b8
MiguelRen <migueleduardorengel@gmail.com>
2023-08-05 23:06:50 -0400

    push/fetch problems

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/github-vite-tesis-project/',
+  
   plugins: [vue(
     // {
     //   template: {
