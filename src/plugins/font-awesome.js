aa484bbb17697273023037e872db253876759fd2
MiguelRen <migueleduardorengel@gmail.com>
2023-08-05 23:39:57 -0400

    fontawsome problem fixing(not used modules)

--- src/plugins/font-awesome.js
+++ src/plugins/font-awesome.js
@@ -1,7 +1,7 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { faHome, faUser, faUserPlus, faSignInAlt, faSignOutAlt } from "@fortAwesome/free-solid-svg-icons";
+// import { faHome, faUser, faUserPlus, faSignInAlt, faSignOutAlt } from "@fortAwesome/free-solid-svg-icons";
     
-library.add( faHome, faUser, faUserPlus, faSignInAlt, faSignOutAlt );
+// library.add( faHome, faUser, faUserPlus, faSignInAlt, faSignOutAlt );
 
 export {FontAwesomeIcon};
