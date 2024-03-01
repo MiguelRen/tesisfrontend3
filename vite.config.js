aaef10235cd563c216e4015ab27abb7f530318f5
MiguelRen <migueleduardorengel@gmail.com>
2024-03-01 15:16:07 -0400

    adding a /

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   
   plugins: [vue()],
-  base:"https://miguelren.github.io/tesisfrontend3",  
+  base:"https://miguelren.github.io/tesisfrontend3/",  
  
 })
