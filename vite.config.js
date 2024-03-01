7fb4f194a60f8600237a07132504fa79add91ccb
MiguelRen <migueleduardorengel@gmail.com>
2024-03-01 13:31:42 -0400

    standarizing  vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-// import vue from 'https://unpkg.com/vue@next'npm install
 
 
 
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  // build: {
-  //   rollupOptions: {
-  //     output: {
-  //       format: 'es',
-  //       plugins: [
-  //         commonjs({
-  //           include: []
-  //         })
-  //       ]
-  //     }
-  //   }
-  // }
-  // ,
   
   plugins: [vue()],
-  
-  
-
-
  
 })
 
